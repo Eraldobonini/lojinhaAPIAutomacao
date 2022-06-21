15d1b572c1427a2b84ffc2917e17102762342d0b
DESKTOP <eraldobonini@hotmail.com>
2022-06-21 15:31:57 -0300

    Inicializando o repositorio com os arquivos iniciais do projeto

---
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -0,0 +1,104 @@
+package modulos.produto;
+
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pojo.UsuarioPojo;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+@DisplayName("Testes de API Rest do modulo de Produto")
+public class ProdutoTest {
+    private String token;
+
+    @BeforeEach
+    public void beforeEach() {
+        // Configurando os dados da API Rest da Lojinha
+        baseURI = "http://165.227.93.41";
+        //port = 8080;
+        basePath = "/lojinha";
+
+        UsuarioPojo usuario = new UsuarioPojo();
+        usuario.setUsuariologin("admin");
+        usuario.setUsuariosenha("admin");
+
+        // Obter o token do usuario Admin
+        this.token = given()
+                .contentType(ContentType.JSON)
+                .body(usuario)
+            .when()
+                .post("/v2/login")
+            .then()
+                .extract()
+                    .path("data.token");
+    }
+
+    @Test
+    @DisplayName("Validar que o valor do produto igual a 0.00 não e permitido")
+    public void testValidarLimitesZeradoProibidoValorProduto() {
+
+
+        // Tentar inserir um produto com valor 0.00 e validar que a mensagem de erro foi apresentada e o
+        // status code retornado foi 422
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("token", this.token)
+                .body("{\n" +
+                        "  \"produtoNome\": \"Play Stion 5\",\n" +
+                        "  \"produtoValor\": 0.00,\n" +
+                        "  \"produtoCores\": [\n" +
+                        "    \"preto\"\n" +
+                        "  ],\n" +
+                        "  \"produtoUrlMock\": \"\",\n" +
+                        "  \"componentes\": [\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"controle\",\n" +
+                        "      \"componenteQuantidade\": 1\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}")
+        .when()
+                .post("/v2/produtos")
+        .then()
+                .assertThat()
+                    .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                    .statusCode(422);
+    }
+
+    @Test
+    @DisplayName("Validar que o valor do produto igual a 7000.01 não e permitido")
+    public void testValidarLimitesteMilProibidoValorProduto() {
+        // Tentar inserir um produto com valor 0.00 e validar que a mensagem de erro foi apresentada e o
+        // status code retornado foi 422
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("token", this.token)
+                .body("{\n" +
+                        "  \"produtoNome\": \"Play Stion 5\",\n" +
+                        "  \"produtoValor\": 7000.01,\n" +
+                        "  \"produtoCores\": [\n" +
+                        "    \"preto\"\n" +
+                        "  ],\n" +
+                        "  \"produtoUrlMock\": \"\",\n" +
+                        "  \"componentes\": [\n" +
+                        "    {\n" +
+                        "      \"componenteNome\": \"controle\",\n" +
+                        "      \"componenteQuantidade\": 1\n" +
+                        "    }\n" +
+                        "  ]\n" +
+                        "}")
+            .when()
+                .post("/v2/produtos")
+            .then()
+                .assertThat()
+                    .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                    .statusCode(422);
+    }
+}
